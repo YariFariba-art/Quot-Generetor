4711ed154fca2d7ed3e5185556d34da10f894159
Fariba Yari <fariba.yari2019@gmail.com>
2025-02-07 10:02:45 +0430

    JS Debug & UX Improve

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-function showAnswer(response) {
-    let quote = response.data.answer;
-    let quoteElement = document.querySelector("#quoteDisplay");
-    quoteElement.innerHTML = `${quote}`; 
-    
-    new Typewriter("#quoteDisplay", {
-      strings: `${quote}`,
-      autoStart: true,
-      cursor: null,
-      delay: 100,
+function showAnswer(response) {  
+  let quote = response.data.answer;
+  let quoteElement = document.querySelector("#quoteDisplay");
+  quoteElement.innerHTML = `${quote}`; 
+  
+  new Typewriter("#quoteDisplay", {
+    strings: `${quote}`,
+    autoStart: true,
+    cursor: null,
     });
-  }
-  
-  function generatedQuote(event) {
-    event.preventDefault();
-    let keyword = document.querySelector("#keyword").value; 
-    let prompt = `please write a quote about the ${keyword}`;
-    let context =
-      "it should be polite, each time unique quote, write the quote straightforward without saying words like sure!";
-    let key = "ef6e0a4b30090aob6a88ad3faec013at";
+}
+function generatedQuote(event) {
+  event.preventDefault();
+  let quoteElement = document.querySelector("#quoteDisplay");
+  quoteElement.innerHTML = "⏳Please wait...";
+  let keyword = document.querySelector("#keyword").value; 
+  let prompt = `write a quote about the ${keyword}`;
+  let context =
+    "quote instructions: it should be short, each time generate a unique quote, it should be positive and motivating,make sure to follow the quote instructions";
+  let key = "ef6e0a4b30090aob6a88ad3faec013at";
   let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+  axios.get(apiurl).then(showAnswer);
+}
 
-    axios.get(apiurl).then(showAnswer);
-  }
-  
-  let form = document.querySelector("#quoteForm");
-  form.addEventListener("submit", generatedQuote); 
-  
+let form = document.querySelector("#quoteForm");
+form.addEventListener("submit", generatedQuote);
